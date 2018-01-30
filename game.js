5f9de8818b210bea5e7dce094431b88d72137d7e
karinakozarova <karinakozarova1303@gmail.com>
2018-01-30 12:34:20 +0200

    beautify code

--- game.js
+++ game.js
@@ -1,107 +1,107 @@
 setInterval('window.location.reload()', 60000);
 
 function myBallDraw() {
-	var canvas = document.getElementById('tutorial');
-	if (canvas.getContext) {
-	  var ctx = canvas.getContext('2d');
-	}
+    var canvas = document.getElementById('tutorial');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+    }
 
-	var clicks;
-	var raf;
-	var streak = 0;
-	function draw() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		
-		ball.draw();
-		ball.x += ball.vx;
-		ball.y += ball.vy;
+    var clicks;
+    var raf;
+    var streak = 0;
 
-	  if(ball.y + ball.radius + ball.vy > canvas.height
-	   || ball.y - ball.radius + ball.vy < 0) {
-	  	ball.vy = -ball.vy;
-	  }
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	  if(ball.x + ball.radius + ball.vx > canvas.width
-	   || ball.x - ball.radius + ball.vx < 0) {
-	  	ball.vx = -ball.vx;
-	  }
+        ball.draw();
+        ball.x += ball.vx;
+        ball.y += ball.vy;
 
-	  raf = window.requestAnimationFrame(draw);
-	}	
+        if (ball.y + ball.radius + ball.vy > canvas.height ||
+            ball.y - ball.radius + ball.vy < 0) {
+            ball.vy = -ball.vy;
+        }
 
-	var p = document.createElement('p');
-	p.id = 'stats';
+        if (ball.x + ball.radius + ball.vx > canvas.width ||
+            ball.x - ball.radius + ball.vx < 0) {
+            ball.vx = -ball.vx;
+        }
 
-	function moveCanvas(padding)
-	{
-		var div = document.createElement('div');
-		div.id = 'div';
-		var body = document.getElementsByTagName('body')[0]; 
-		body.insertBefore(div, body.childNodes[2]);
-		div.appendChild(canvas);
-		div.style.padding = "" + padding + "px";
-		console.log(padding);
-	}
+        raf = window.requestAnimationFrame(draw);
+    }
 
-	function randomIntFromInterval(min,max){
-	    return Math.floor(Math.random()*(max-min+1)+min);
-	}
+    var p = document.createElement('p');
+    p.id = 'stats';
 
-	function changeVelocity(){
-		ball.vx *= randomIntFromInterval(1,5);
-		ball.vy *= randomIntFromInterval(1,5);
-		ball.vy *= -1;
-		ball.vx *= -1;
-	}
+    function moveCanvas(padding) {
+        var div = document.createElement('div');
+        div.id = 'div';
+        var body = document.getElementsByTagName('body')[0];
+        body.insertBefore(div, body.childNodes[2]);
+        div.appendChild(canvas);
+        div.style.padding = "" + padding + "px";
+        console.log(padding);
+    }
 
-	var ball = {
-	  x: 100,
-	  y: 100,
-	  vx: 5,
-	  vy: 1,
-	  radius: 80,
-	  color: 'yellow',
-	  is_mouse_over: function(x,y) {
-	  	return ((this.x - radius) < x < (this.x + radius)) 
-	  		&& ((this.y - radius) < y < (this.y + radius));
-	  },
-	  draw: function() {
-	    ctx.beginPath();
-	    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-	    ctx.closePath();
-	    ctx.fillStyle = this.color;
-	    ctx.fill();
-	  }
-	};
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
 
-	canvas.addEventListener('click', function(e){
-		var change = (10*ball.radius)/100;
-		if((e.x - change < ball.x + ball.radius && e.x - change > ball.x - ball.radius)
-		&& (e.y - change < ball.y + ball.radius && e.y - change > ball.y - ball.radius)){
-			console.log("if");
-			ball.radius *= .8;
-			streak++;
-			clicks++;
-			changeVelocity();
-			ball.color = "white";
-		} else {
-			console.log("else");
-			streak = 0;
-			ball.vx *= 0.8;
-			ball.color = "red";
-		}
+    function changeVelocity() {
+        ball.vx *= randomIntFromInterval(1, 5);
+        ball.vy *= randomIntFromInterval(1, 5);
+        ball.vy *= -1;
+        ball.vx *= -1;
+    }
 
-		if(streak == 3){
-			location.reload();
-			alert("You win!");
-		}
+    var ball = {
+        x: 100,
+        y: 100,
+        vx: 5,
+        vy: 1,
+        radius: 80,
+        color: 'yellow',
+        is_mouse_over: function(x, y) {
+            return ((this.x - radius) < x < (this.x + radius)) &&
+                ((this.y - radius) < y < (this.y + radius));
+        },
+        draw: function() {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+            ctx.closePath();
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+    };
 
-		document.getElementById("stats").innerHTML = " Streak: " + streak;
+    canvas.addEventListener('click', function(e) {
+        var change = (10 * ball.radius) / 100;
+        if ((e.x - change < ball.x + ball.radius && e.x - change > ball.x - ball.radius) &&
+            (e.y - change < ball.y + ball.radius && e.y - change > ball.y - ball.radius)) {
+            console.log("if");
+            ball.radius *= .8;
+            streak++;
+            clicks++;
+            changeVelocity();
+            ball.color = "white";
+        } else {
+            console.log("else");
+            streak = 0;
+            ball.vx *= 0.8;
+            ball.color = "red";
+        }
 
-	});
-	//setInterval(moveCanvas(randomIntFromInterval(1, 500)), 1000);
-	canvas.style.backgroundImage = "url(https://sports.cbsimg.net/images/nhl/blog/NHL_Patrick_Kane_Investigation.jpg)";
-	draw();
-	
+        if (streak == 3) {
+            location.reload();
+            alert("You win!");
+        }
+
+        document.getElementById("stats").innerHTML = " Streak: " + streak;
+
+    });
+    //setInterval(moveCanvas(randomIntFromInterval(1, 500)), 1000);
+    canvas.style.backgroundImage = "url(https://sports.cbsimg.net/images/nhl/blog/NHL_Patrick_Kane_Investigation.jpg)";
+    draw();
+
 }
 myBallDraw();
